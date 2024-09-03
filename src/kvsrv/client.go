41fee89b729e10f9682fd2dcf1973889b0e24eb8
ziangtian <bienlor@163.com>
2024-09-03 22:34:56 +0800

    sync lab2 while starting

--- src/kvsrv/client.go
+++ src/kvsrv/client.go
@@ -1,9 +1,11 @@
 package kvsrv
 
-import "6.5840/labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"math/big"
 
+	"6.5840/labrpc"
+)
 
 type Clerk struct {
 	server *labrpc.ClientEnd
@@ -36,7 +38,13 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
 
-	// You will have to modify this function.
+	args := &GetArgs{Key: key}
+	reply := &GetReply{}
+
+	ck.server.Call("KVServer.Get", &args, &reply)
+	if reply.Exists {
+		return reply.Value
+	}
 	return ""
 }
 
@@ -50,6 +58,9 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) string {
 	// You will have to modify this function.
+	args := &PutAppendArgs{Key: key, Value: value}
+	reply := &PutAppendReply{}
+
 	return ""
 }
 
