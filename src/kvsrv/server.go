4e4a3d6da696b029ad043e3ed5840edc369422d6
ziangtian <bienlor@163.com>
2024-09-04 21:21:59 +0800

    lab2 server still using too much memory

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -18,9 +18,9 @@ type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
-	kvpairs   map[string]string
-	reqStatus map[int64]bool   // whether a request has been fulfilled
-	reqReply  map[int64]string // records the correct reply for a request
+	kvpairs map[string]string
+	// reqStatus map[int64]bool   // whether a request has been fulfilled
+	reqReply map[int64]string // records the correct reply for a request
 }
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
@@ -30,12 +30,12 @@ func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// if is not resend, assert the request has not been fulfilled
 	if !args.IsResend {
 		// search in reqStatus and reqReply, should not find the request
-		_, ok1 := kv.reqStatus[args.ReqId]
+		// _, ok1 := kv.reqStatus[args.ReqId]
 		_, ok2 := kv.reqReply[args.ReqId]
-		if ok1 || ok2 {
+		if ok2 {
 			panic("A fresh request HAD been fulfilled")
 		}
-		kv.reqStatus[args.ReqId] = true
+		// kv.reqStatus[args.ReqId] = true
 
 		value, ok := kv.kvpairs[args.Key]
 		if ok {
@@ -46,7 +46,8 @@ func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 		kv.reqReply[args.ReqId] = reply.Value
 	} else {
 		// if is resend, check if the request has been fulfilled
-		fulfilled := kv.reqStatus[args.ReqId]
+		// fulfilled := kv.reqStatus[args.ReqId]
+		_, fulfilled := kv.reqReply[args.ReqId]
 		if !fulfilled { // if request has not been fulfilled
 			// fulfill the request
 			value, ok := kv.kvpairs[args.Key]
@@ -56,7 +57,7 @@ func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 				reply.Value = ""
 			}
 			kv.reqReply[args.ReqId] = reply.Value
-			kv.reqStatus[args.ReqId] = true
+			// kv.reqStatus[args.ReqId] = true
 		} else {
 			// if request has been fulfilled, return the correct reply
 			reply.Value = kv.reqReply[args.ReqId]
@@ -76,24 +77,25 @@ func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	defer kv.mu.Unlock()
 	DPrintf("Got Put request: %s %s", args.Key, args.Value)
 	if !args.IsResend { // if is fresh
-		_, ok1 := kv.reqStatus[args.ReqId]
+		// _, ok1 := kv.reqStatus[args.ReqId]
 		_, ok2 := kv.reqReply[args.ReqId]
-		if ok1 || ok2 {
+		if ok2 {
 			panic("A fresh request HAD been fulfilled")
 		}
 
-		kv.reqStatus[args.ReqId] = true
+		// kv.reqStatus[args.ReqId] = true
 		kv.reqReply[args.ReqId] = ""
 		kv.kvpairs[args.Key] = args.Value
 
 		reply.Value = ""
 	} else {
 		// if is resend, check if the request has been fulfilled
-		fulfilled := kv.reqStatus[args.ReqId]
+		// fulfilled := kv.reqStatus[args.ReqId]
+		_, fulfilled := kv.reqReply[args.ReqId]
 		if !fulfilled { // if request has not been fulfilled
 			kv.kvpairs[args.Key] = args.Value
 			kv.reqReply[args.ReqId] = ""
-			kv.reqStatus[args.ReqId] = true
+			// kv.reqStatus[args.ReqId] = true
 			reply.Value = ""
 		} else {
 			// if request has been fulfilled, return the correct reply
@@ -107,9 +109,9 @@ func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
 	if !args.IsResend {
-		_, ok1 := kv.reqStatus[args.ReqId]
+		// _, ok1 := kv.reqStatus[args.ReqId]
 		_, ok2 := kv.reqReply[args.ReqId]
-		if ok1 || ok2 {
+		if ok2 {
 			panic("A fresh request HAD been fulfilled")
 		}
 
@@ -122,11 +124,12 @@ func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 			reply.Value = oldValue
 		}
 
-		kv.reqStatus[args.ReqId] = true
+		// kv.reqStatus[args.ReqId] = true
 		kv.reqReply[args.ReqId] = reply.Value
 	} else {
 		// if is resend, check if the request has been fulfilled
-		fulfilled := kv.reqStatus[args.ReqId]
+		// fulfilled := kv.reqStatus[args.ReqId]
+		_, fulfilled := kv.reqReply[args.ReqId]
 		if !fulfilled { // if request has not been fulfilled
 			oldValue, ok := kv.kvpairs[args.Key]
 			if !ok { // key not exist
@@ -137,7 +140,7 @@ func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 				reply.Value = oldValue
 			}
 
-			kv.reqStatus[args.ReqId] = true
+			// kv.reqStatus[args.ReqId] = true
 			kv.reqReply[args.ReqId] = reply.Value
 		} else {
 			// if request has been fulfilled, return the correct reply
@@ -153,7 +156,7 @@ func StartKVServer() *KVServer {
 	// You may need initialization code here.
 	kv.kvpairs = make(map[string]string)
 	kv.mu = sync.Mutex{}
-	kv.reqStatus = make(map[int64]bool)
+	// kv.reqStatus = make(map[int64]bool)
 	kv.reqReply = make(map[int64]string)
 	return kv
 }
